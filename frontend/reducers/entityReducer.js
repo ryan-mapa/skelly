655bf1e321343535f4970614f2b6d2e74c5a7a13
ryan-mapa <ryan.m.mapa@gmail.com>
2021-02-05 14:21:30 -0800

    add entityReducer example

---
+++ frontend/reducers/entityReducer.js
@@ -0,0 +1,11 @@
+const entityReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    // case CASE:
+    //   return state;
+    default:
+      return state;
+  }
+};
+
+export default entityReducer;
